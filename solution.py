fa103cd6a8cb1ddcab11fd62c175738278bf0664
Jcom619 <joshuacompton@my.smccd.edu>
2021-03-31 09:07:48 -0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,38 @@
-## The Problem You'll Be Interviewing Someone On
+# # The Problem You'll Be Interviewing Someone On
 
-Make a `solution.py` that solves the following problem: given a string representing a language (we'll plan for "english", "spanish", and "french"), print out a greeting for that language (in this case: "Hello!" and "Hola!" and "Bonjour!").
+# Make a `solution.py` that solves the following problem: given a string representing a language (we'll plan for "english", "spanish", and "french"), print out a greeting for that language (in this case: "Hello!" and "Hola!" and "Bonjour!").
 
-## Basic Run-Through
+# # Basic Run-Through
 
-- Ask the user running the program (using `input()`) for a language. You should NOT assume it will come in any particular case (they could enter it in uppercase or lowercase or mixed), but you CAN assume it'll only be one word.
-- Include some logic to choose from the right greeting based on that input.
-- Print out that greeting!
+# - Ask the user running the program(using `input()`) for a language. You should NOT assume it will come in any particular case(they could enter it in uppercase or lowercase or mixed), but you CAN assume it'll only be one word.
+# - Include some logic to choose from the right greeting based on that input.
+# - Print out that greeting!
 
-## Streeeeeetch Goal
+# # Streeeeeetch Goal
 
-- Did you use an `if`? What if we have 20 languages? 100? You don't want to write all those `elif`s! Think about how you could use a dictionary with keys. Each key could be a language, and each value, the... ??
+# - Did you use an `if`? What if we have 20 languages? 100? You don't want to write all those `elif`s! Think about how you could use a dictionary with keys. Each key could be a language, and each value, the... ??
+greeting_lang = {
+    "e_greet": "Hello!",
 
+    "s_greet": "Hola!",
+
+    "f_greet": "Bonjour!"
+}
+
+e = greeting_lang['e_greet']
+s = greeting_lang['s_greet']
+f = greeting_lang['f_greet']
+
+make_selection = input("enter number and press return:\n")
+
+
+def greet_response(make_selection):
+    if (make_selection == "1"):
+        print(e)
+    elif(make_selection == "2"):
+        print(s)
+    else:
+        print(f)
+
+
+greet_response(make_selection)
