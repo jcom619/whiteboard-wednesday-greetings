b04ea7b64e5a3924cbbdf7e931748d527aa38a85
Jcom619 <joshuacompton@my.smccd.edu>
2021-03-31 19:06:43 -0700

    jc

--- solution.py
+++ solution.py
@@ -1,45 +1,14 @@
-# # The Problem You'll Be Interviewing Someone On
+# 2 num print every num btween  the 2 num including the 2 avoid  anythin with a 5
+# [ 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20 ]
+question = input("provide two whole numbers")
+numbers = question.split()
 
-# Make a `solution.py` that solves the following problem: given a string representing a language (we'll plan for "english", "spanish", and "french"), print out a greeting for that language (in this case: "Hello!" and "Hola!" and "Bonjour!").
+num_one = 1
+num_two = 20
 
-# # Basic Run-Through
-
-# - Ask the user running the program(using `input()`) for a language. You should NOT assume it will come in any particular case(they could enter it in uppercase or lowercase or mixed), but you CAN assume it'll only be one word.
-# - Include some logic to choose from the right greeting based on that input.
-# - Print out that greeting!
-
-# # Streeeeeetch Goal
-
-# - Did you use an `if`? What if we have 20 languages? 100? You don't want to write all those `elif`s! Think about how you could use a dictionary with keys. Each key could be a language, and each value, the... ??
-
-# todo USED DICTIONARY TO SIMPLIFY SELECTION OF LANGUAGE
-greeting_lang = {
-    "e_greet": "Hello!",
-
-    "s_greet": "Hola!",
-
-    "f_greet": "Bonjour!"
-}
-
-# todo VARS ASSIGNED TO SELECT DICT. ITEMS EASILY
-e = greeting_lang['e_greet']
-s = greeting_lang['s_greet']
-f = greeting_lang['f_greet']
-
-# tod CHOSE NUMBERS AS A TEMPORARY BRUTEFORCE RESPONSE FOR AVOIDING ANY POSSIBLE CAPS PROBLEMS
-
-make_selection = input("\ntype your language and press return:\n")
-
-
-def greet_response(make_selection):
-    if(make_selection.lower() == "english"):
-        print(e)
-    elif(make_selection.lower() == "spanish"):
-        print(s)
-    elif(make_selection.lower() == "french"):
-        print(f)
-    else:
-        print("make sure your selection is within the parameters of 1, 2 or 3!")
-
-
-greet_response(make_selection)
+# container = []
+# if num_one in question
+for num in range(num_one, num_two):
+    # if num
+    # response
+    # # print()
---
+++ solution1.py
@@ -0,0 +1,57 @@
+# # The Problem You'll Be Interviewing Someone On
+
+# Make a `solution.py` that solves the following problem: given a string representing a language (we'll plan for "english", "spanish", and "french"), print out a greeting for that language (in this case: "Hello!" and "Hola!" and "Bonjour!").
+
+# # Basic Run-Through
+
+# - Ask the user running the program(using `input()`) for a language. You should NOT assume it will come in any particular case(they could enter it in uppercase or lowercase or mixed), but you CAN assume it'll only be one word.
+# - Include some logic to choose from the right greeting based on that input.
+# - Print out that greeting!
+
+# # Streeeeeetch Goal
+
+# - Did you use an `if`? What if we have 20 languages? 100? You don't want to write all those `elif`s! Think about how you could use a dictionary with keys. Each key could be a language, and each value, the... ??
+
+
+# todo: get them unstuck, Give them hints via, "What if you did this?"
+
+# todo: Encourage them to - Talk through the problem, Don't give up, Ask about edge cases
+
+# todo: If they're thinking list, let them explore it (spoiler alert: it won't be pretty)
+
+# todo: If they struggle to make a dictionary, that's not uncommon at this point, and this is great practice for them.
+
+# todo: Walk them through the key/value structure
+
+
+# I USED DICTIONARY TO SIMPLIFY SELECTION OF LANGUAGE
+greeting_lang = {
+    "e_greet": "Hello!",
+
+    "s_greet": "Hola!",
+
+    "f_greet": "Bonjour!"
+}
+
+# I USED VARS ASSIGNED TO SELECT DICT. ITEMS EASILY
+e = greeting_lang['e_greet']
+s = greeting_lang['s_greet']
+f = greeting_lang['f_greet']
+
+# originally CHOSE NUMBERS AS A TEMPORARY BRUTEFORCE RESPONSE FOR AVOIDING ANY POSSIBLE CAPS PROBLEMS
+
+make_selection = input("\ntype your language and press return:\n")
+
+
+def greet_response(make_selection):
+    if(make_selection.lower() == "english"):
+        print(e)
+    elif(make_selection.lower() == "spanish"):
+        print(s)
+    elif(make_selection.lower() == "french"):
+        print(f)
+    else:
+        print("make sure your selection is within the parameters of 1, 2 or 3!")
+
+
+greet_response(make_selection)
